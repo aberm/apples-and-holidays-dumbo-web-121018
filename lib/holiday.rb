f79d7c4b1a2f75f85898d590e10ffb9e4bb92c58
Menachem Aber <abermendel@gmail.com>
2018-11-27 05:58:26 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holidays|
     if season == "winter"
       holidays.each do |name, supplies|
-        supplies.values.flatten
+        for i in supplies do
       end
     end
   end
