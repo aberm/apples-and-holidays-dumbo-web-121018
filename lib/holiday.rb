2aa10980b0caf4020f5dd3beece70d5a77202a04
Menachem Aber <abermendel@gmail.com>
2018-11-27 05:52:22 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,6 +88,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |name, supplies|
       name if supplies.include?("BBQ")
+    end
+  end
 end
 
 
